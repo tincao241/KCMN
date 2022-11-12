ecf03a81e4f79df614ad45b8a5109f123235d1ea
Tín Đặng Trung <tincao241@gmail.com>
2022-11-12 16:24:53 +0700

    Pls

---
+++ Bai1.java
@@ -0,0 +1,3 @@
+list1, list2 = [123, 'xyz', 'zara', 'abc'], [456, 700, 200]
+print "min value element : ", min(list1)
+print "min value element : ", min(list2)
---
+++ Ontap.py
@@ -0,0 +1,5 @@
+list1 =  [1, 2, 3]
+list2 = ['1', 'xyz', 'zara', 'abc']
+
+print ("min value element : "), min(list1)
+print ("min value element : "), min(list2)
